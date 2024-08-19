633947e94eb20b0e9740700bb9ca1689c4cea076
Chloe Jiang <yueyue426@berkeley.edu>
2024-08-18 18:53:09 -0700

    ceate user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match a valid email address'],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+// Create a virtual property to retrieves the length of the user's friends array
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// Initialize the User model
+const User = model('user', userSchema);
+
+module.exports = User;
